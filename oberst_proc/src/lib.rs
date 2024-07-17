14f9f5a30c4dd11ad6702f6942cd22c0b73189a0
lionrexhepi <lion.rexhepi@gmx.de>
2024-07-17 14:03:48 +0200

    command functions must now take their respective context as first argument

--- oberst_proc/src/lib.rs
+++ oberst_proc/src/lib.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
 use proc_macro::{Span, TokenStream};
-use quote::quote;
-use syn::{braced, parse_macro_input, spanned::Spanned, Attribute, Ident, ItemFn, Signature, Type};
+use quote::{quote, ToTokens};
+use syn::{
+    braced, parenthesized, parse_macro_input, spanned::Spanned, Attribute, Error, FnArg, Ident,
+    ItemFn, Pat, PatType, Signature, Type, TypeReference,
+};
 
 #[proc_macro]
 pub fn define_command(input: TokenStream) -> TokenStream {
@@ -81,13 +84,16 @@ struct CommandDefiniton {
 impl syn::parse::Parse for CommandDefiniton {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let name = input.parse()?;
-        input.parse::<syn::Token![:]>()?;
-        let context_type = input.parse()?;
+        let context_type;
+        parenthesized!(context_type in input);
+        let context_type: Type = context_type.parse()?;
+
         let variant_block;
         braced!(variant_block in input);
         let mut variants = vec![];
         while !variant_block.is_empty() {
             let mut function = variant_block.parse::<syn::ItemFn>()?;
+            check_context_arg(&function.sig, &context_type)?;
             let arg_names = extract_args_from_signature(&function.sig)?;
 
             let syntax =
@@ -138,14 +144,14 @@ fn extract_args_from_signature(sig: &Signature) -> syn::Result<HashMap<Ident, Ty
         .iter()
         .skip(1)
         .map(|arg| {
-            if let syn::FnArg::Typed(pat) = arg {
-                if let syn::Pat::Ident(ident) = &*pat.pat {
+            if let FnArg::Typed(pat) = arg {
+                if let Pat::Ident(ident) = &*pat.pat {
                     Ok((ident.ident.clone(), *pat.ty.clone()))
                 } else {
-                    return Err(syn::Error::new(pat.pat.span(), "Expected identifier"));
+                    return Err(Error::new(pat.pat.span(), "Expected identifier"));
                 }
             } else {
-                return Err(syn::Error::new(arg.span(), "Expected typed argument"));
+                return Err(Error::new(arg.span(), "Expected typed argument"));
             }
         })
         .collect::<syn::Result<HashMap<_, _>>>()
@@ -170,7 +176,7 @@ fn extract_usage_string_from_metadata(attrs: &mut Vec<Attribute>) -> syn::Result
                     break;
                 }
                 _ => {
-                    return Err(syn::Error::new(attr.span(), "Expected usage string"));
+                    return Err(Error::new(attr.span(), "Expected usage string"));
                 }
             }
         }
@@ -208,4 +214,23 @@ fn build_syntax_from_usage(arg_names: &HashMap<Ident, Type>, usage: String) -> V
             }
         })
         .collect();
+}
+
+fn check_context_arg(sig: &Signature, context_type: &Type) -> syn::Result<()> {
+    match sig.inputs.first() {
+        Some(FnArg::Typed(PatType { ty, .. })) => match &**ty {
+            Type::Reference(TypeReference { elem, .. })
+                if elem.to_token_stream().to_string()
+                    == context_type.to_token_stream().to_string() =>
+            // Since syn::Type doesn't implement PartialEq, we have to convert to a string
+            {
+                Ok(())
+            }
+            _ => Err(Error::new(ty.span(), "Expected reference to context type")),
+        },
+        _ => Err(Error::new(
+            sig.inputs.span(),
+            "Expected reference to context type",
+        )),
+    }
 }
